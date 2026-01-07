07c30f05baed5f75d6f7aedfd73edf7769ce3984
海蜇 <156986934+GuoHaiZhe12138@users.noreply.github.com>
2026-01-07 18:45:24 +0800

    清理代码、添加示例工程 (#2)
    * code clean
    
    * clean code/add example project
    
    guo review ok

---
+++ Examples/Stm32f103c8t6/main.c
@@ -0,0 +1,128 @@
+#include "stm32f10x.h"
+#include "OtaInterface.h"
+
+void UART1_Init(void);
+void UART2_Init(void);
+void PB11_Input_Init(void);
+uint8_t OTA_DebugSend(const char *data);
+	
+int main(void)
+{
+	UART1_Init();
+	UART2_Init();
+	PB11_Input_Init();
+	OTA_Run();
+	while(1)
+	{
+	};
+}
+
+void PB11_Input_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    // 1. 使能 GPIOB 的时钟
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+    // 2. 配置引脚参数
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_11;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPD;   // 输入模式
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;    // 选择下拉
+    
+    // 3. 执行初始化
+    GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+void UART1_Init(void)
+{
+    GPIO_InitTypeDef  GPIO_InitStructure;
+    USART_InitTypeDef USART_InitStructure;
+    NVIC_InitTypeDef  NVIC_InitStructure;
+
+    /* 1. 时钟使能 */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | 
+                           RCC_APB2Periph_GPIOA  | 
+                           RCC_APB2Periph_AFIO, ENABLE);
+
+    /* 2. GPIO 配置 */
+    /* PA9  -> USART1_TX */
+    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* PA10 -> USART1_RX */
+    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* 3. USART 参数配置 */
+    USART_InitStructure.USART_BaudRate   = 9600;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits   = USART_StopBits_1;
+    USART_InitStructure.USART_Parity     = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode       = USART_Mode_Rx | USART_Mode_Tx;
+
+    USART_Init(USART1, &USART_InitStructure);
+
+    /* 4. 使能 RX 中断 */
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+    /* 5. 配置 NVIC */
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+ 
+    /* 6. 使能 USART */
+    USART_Cmd(USART1, ENABLE);
+}
+
+void USART1_IRQHandler(void)
+{
+    if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+    {
+		uint8_t ch = (uint8_t)USART_ReceiveData(USART1);
+		
+		OTA_ReceiveTask(ch);
+    }
+}
+
+void UART2_Init(void)
+{
+    /* 关键：开启 USART2 时钟（位于 APB1） */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+    /* GPIOA 时钟 */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    USART_InitTypeDef USART_InitStructure;
+
+    /* PA2 TX */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* PA3 RX */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* USART2 参数 */
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+    USART_Init(USART2, &USART_InitStructure);
+
+    /* 使能 USART2 */
+    USART_Cmd(USART2, ENABLE);
+}
